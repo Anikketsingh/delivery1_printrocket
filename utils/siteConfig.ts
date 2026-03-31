7b967e0cb6b370aa61b07f287ad195b9d062c6a5
Aniket Singh <anikket.singh@Aniket-MacBook-Air.local>
2026-04-01 01:12:37 +0530

    final

--- utils/siteConfig.ts
+++ utils/siteConfig.ts
@@ -1,20 +1,22 @@
 export const siteConfig = {
   siteName: "PrintRocket",
+
   vendorDashboardUrl:
     process.env.NEXT_PUBLIC_VENDOR_DASHBOARD_URL ||
-    "https://print-rocket-web-nextjs-aikg.vercel.app/",
+    "https://print-rocket-web-nextjs-aikg.vercel.app",
+
   adminDashboardBaseUrl:
     process.env.NEXT_PUBLIC_ADMIN_DASHBOARD_BASE_URL ||
-    "https://print-rocket-admin-panel-2446.vercel.app/",
-  adminSignUpKeyPath:
-    process.env.NEXT_PUBLIC_ADMIN_SIGNUP_KEY_PATH ||
-    "/$2b$10$M0mdRRBS4YUlMcjPQKYL3O/bhHKTfJIDQTKuvvor532NX2mUYWqZm",
-  adminPanelUrl:
-    (process.env.NEXT_PUBLIC_ADMIN_DASHBOARD_BASE_URL ||
-      "https://print-rocket-admin-panel-2446.vercel.app/") +
-    (process.env.NEXT_PUBLIC_ADMIN_SIGNUP_KEY_PATH ||
-      "/$2b$10$M0mdRRBS4YUlMcjPQKYL3O/bhHKTfJIDQTKuvvor532NX2mUYWqZm"),
-  apkPublicPath: "/apk/printrocket-fe.apk",
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3001"
-};
+    "https://print-rocket-admin-panel-2446.vercel.app",
 
+  adminPanelUrl:
+    process.env.NEXT_PUBLIC_ADMIN_DASHBOARD_BASE_URL ||
+    "https://print-rocket-admin-panel-2446.vercel.app",
+
+  apkPublicPath:
+    process.env.NEXT_PUBLIC_APK_URL ||
+    "https://github.com/Anikketsingh/delivery1_printrocket/releases/download/v1test/printrocket-fe.apk",
+
+  siteUrl:
+    process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3001"
+};
